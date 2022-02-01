6fb8418b5960d02b063ea6cfa68a357107244848
Sodaman <mattrick2214@gmail.com>
2022-02-01 13:10:14 -0600

    using routes now to separate tables, getting back still random view all employees without asking

---
+++ server.js
@@ -0,0 +1,67 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+});
+
+
+// const promptUser = () => {
+//     console.log(`
+//     *record scratch*
+//     `);
+//     inquirer.prompt([
+//         {
+//             type: 'list',
+//             name: 'mainQuestion',
+//             message: 'What would you like to do?',
+//             choices: ['View All Employees', 'Add an Employee', 'Update an Employee', 'Remove an Employee', 'View All Positions', 'Add a position', 'Remove a Position', 'View all Departments', 'Add a Department', 'Remove a Department', 'Exit!']
+//         }
+//     ])
+//     .then((answers) => {
+//         const {choices} = answers;
+
+//         if (choices === 'View All Employees') {
+//             viewAllEmployees();
+//         }
+//         if (choices === 'Add an Employee') {
+//             addAnEmployee();
+//         }
+//         if (choices === 'Exit') {
+//             connection.end();
+//         }
+        
+//     });
+// };
+
+
+// promptUser();
+
+// viewAllEmployees = () => {
+    
+// }
+
+
+// db.query(`SELECT * FROM employee`, (err, rows) => {
+//     console.table(rows)
+//     // promptUser();
+// });
