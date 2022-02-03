38566a18d026965bdfd7d66f98c635c0b8d2bf75
Sodaman <mattrick2214@gmail.com>
2022-02-03 15:40:34 -0600

    factored out express

--- server.js
+++ server.js
@@ -2,26 +2,10 @@ const db = require('./db/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const apiRoutes = require('./routes/apiRoutes');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use('/api', apiRoutes);
-
-app.use((req, res) => {
-    res.status(404).end();
-});
-
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    promptUser();
 });
 
 const promptUser = () => {
@@ -52,8 +36,6 @@ const promptUser = () => {
     });
 };
 
-
-promptUser();
 
 const viewAllEmployees = () => {
     db.query(`SELECT * FROM employee`, (err, rows) => {
